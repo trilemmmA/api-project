699a85b90b175889510241b38193ff36f8f79266
satsu <supernigger911@icloud.com>
2026-02-01 01:56:25 +0300

    added CLI functions

--- src/tui.py
+++ src/tui.py
@@ -1,5 +1,7 @@
 from models import User, UserAnalyzer
 from storage import *
+from rich.console import Console
+from rich.table import Table
 
 class App:
     def __init__(self, client, storage):
@@ -7,6 +9,7 @@ class App:
         self.storage = storage
         self.users = []
         self.analyzer = UserAnalyzer(self.users)
+        self.console = Console()
         
     def load_users(self):
         raw = self.client.fetch_users()
@@ -16,11 +19,25 @@ class App:
         return self.analyzer
  
     def show_menu(self):
-        print("\nMenu")
-        print("1 - Load Users")
-        print("2 - Search")
-        print("3 - Show all")
-        print("4 - Exit")
+        self.console.rule("[bold cyan]Analyzer[/bold cyan]")
+        
+        self.console.print("[bold]1[/bold] - Load Users")
+        self.console.print("[bold]2[/bold] - Search")
+        self.console.print("[bold]3[/bold] - Show all")
+        self.console.print("[bold]4[/bold] - Exit")
+    
+    def show_rich(self, users):
+        table = Table(title="Users")
+        
+        table.add_column("ID")
+        table.add_column("Name")
+        table.add_column("Email")
+        table.add_column("City")
+        
+        for user in users:
+            table.add_row(str(user.id), user.name, user.email, user.city)
+            
+        self.console.print(table)
         
     def handle_choice(self):
         self.show_menu()
@@ -33,15 +50,14 @@ class App:
             
             elif choice == "2":            
                 if not self.users:
-                    print("Load users first")
+                    self.console.print("[yellow]Load users first[/yellow]")
                     continue
                 
                 pattern = input("Enter search pattern: ")
             
                 result = self.analyzer.search(pattern)            
                        
-                for user in result:
-                    print(user)
+                self.show_rich(result)
                 
                 self.storage.save(result)
                                             
@@ -49,10 +65,9 @@ class App:
                 saved_data = self.storage.load()
             
                 if not saved_data:
-                    print("History is empty")
+                    self.console.print("[bold red]History is empty[/bold red]", justify="center")
                 
-                for user in saved_data:
-                    print(user)
+                self.show_rich(saved_data)
             
             elif choice == "4":
                 break
